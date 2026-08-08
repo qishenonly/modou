fd78a1edde1f8a4e79027ec2afbcf33d45317f91
godqi <1050026498@qq.com>
2026-08-08 18:21:01 +0800

    chore: 版本号 bump 到 0.11.0（版本收尾）

--- packages/tui/src/index.ts
+++ packages/tui/src/index.ts
@@ -69,7 +69,7 @@ import { derivePermissionMode, type TokenTotals } from './status';
 import { createEventChannel } from './stream';
 import { assembleTuiStartup, type TuiOptions } from './startup';
 
-export const version = '0.10.0';
+export const version = '0.11.0';
 
 /** runTui 的产出。 */
 export interface TuiResult {
